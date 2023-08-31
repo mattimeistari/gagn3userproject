1bb0c4f3ab9baa031df1be5fcd008d6904370928
Matthías Madsen Hauksson - FB <FB687789@fb.is>
2023-08-31 11:09:42 +0000

    asd

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 import sqlite3
 
 
-def doNothing():
+# Create placeholder
+def placeholderText():
     print("WTF!")
     return
 
@@ -13,8 +14,8 @@ conn = sqlite3.connect("./db/users.db")
 cursor = conn.cursor()
 
 # Prompt the user to print out the users
-print("Do you wanna print all the peopls (Y/N)")
-choice = input("Answer here:")
+print("Do you wanna print all the peopls? (Y/N)")
+choice = input("\nAnswer here:")
 
 # If the user chooses to print the users
 if choice.lower() == "y":
@@ -29,13 +30,20 @@ if choice.lower() == "y":
     for row in rows:
         print(
             f"Name: {row[0]}, Full name: {row[1]} {row[2]}, "
-            f"Email: {row[3]} Flag value: {row[4]}"
+            f"Email: {row[3]} Flag value: {row[4]}\n"
         )
 
-if choice.lower() == "n":
-    doNothing()
+# If he chooses not to, run option 2, which is placeholder
+elif choice.lower() == "n":
+    placeholderText()
 
+# If user fails to inster y/n
+else:
+    print("User Failed to follow instructions")
+
+# End the forrit
 cursor.close()
 conn.close()
-print()
+
+# Closing message
 print("######## FORRIT END ########")
